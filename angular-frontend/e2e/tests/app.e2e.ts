05fc8efa7f4d42b65006d3a85609cad1860574cb
Sebastian Kühne <sebastian.kuehne@outlook.com>
2024-05-19 23:43:55 +0200

    Fixed e2e test

--- angular-frontend/e2e/tests/app.e2e.ts
+++ angular-frontend/e2e/tests/app.e2e.ts
@@ -1,13 +1,11 @@
-
-import {setupBrowserHooks, getBrowserState} from './utils';
+import { getBrowserState, setupBrowserHooks } from './utils';
 
 describe('App test', function () {
   setupBrowserHooks();
   it('is running', async function () {
-    const {page} = getBrowserState();
-    const element = await page.locator('::-p-text(blog)').wait();
+    const { page } = getBrowserState();
+    const element = await page.locator('app-list-teasers').wait();
 
     expect(element).not.toBeNull();
-
   });
 });
