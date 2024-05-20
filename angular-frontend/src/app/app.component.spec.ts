cc525d8a8fb94ae2d7096e1ef3622e9ba53a4b5c
Sebastian Kühne <sebastian.kuehne@outlook.com>
2024-05-20 11:07:55 +0200

    Fixed typo

--- angular-frontend/src/app/app.component.spec.ts
+++ angular-frontend/src/app/app.component.spec.ts
@@ -1,5 +1,5 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {TestBed} from '@angular/core/testing';
+import {AppComponent} from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -17,6 +17,6 @@ describe('AppComponent', () => {
   it(`should have the 'blog' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('My Blog');
+    expect(app.title).toEqual('My Personal Blog');
   });
 });
