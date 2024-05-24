9d7e9006884758243c4b9f2c7776f39c18e8fb25
Sebastian Kühne <skuehne83@gmail.com>
2024-05-24 20:28:22 +0200

    Use Material 3 theme

--- angular-frontend/src/app/components/page-header/page-header.component.ts
+++ angular-frontend/src/app/components/page-header/page-header.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-page-header',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule, RouterLink],
+  imports: [MatButtonModule, MatIconModule, RouterLink, MatToolbarModule],
   templateUrl: './page-header.component.html',
   styleUrl: './page-header.component.scss',
 })
